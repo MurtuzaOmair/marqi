a88d4c67319197517d806b5dd0160e89e703751d
Syed Omair Murtuza <omairmurtuzas@gmail.com>
2024-07-02 01:02:07 +0300

    habibi

--- components/Video.js
+++ components/Video.js
@@ -1,6 +1,3 @@
-import Video from "next-video";
-import heroVideo from "@/videos/11.webm";
-
 const Vid = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
@@ -8,17 +5,6 @@ const Vid = () => {
         <source src="/final-images/marqi.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      {/* 
-      <video
-        autoPlay
-        loop
-        style={{ width: "100vw", height: "500px", objectFit: "cover" }}
-      >
-        <source src="/final-images/11.webm" type="video/webm" />
-      </video> 
-      */}
-
-      {/* <Video src={heroVideo} /> */}
     </div>
   );
 };
