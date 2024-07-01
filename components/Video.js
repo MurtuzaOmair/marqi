fe34c0010c026b32503e1eed3b39849668afeca2
Syed Omair Murtuza <omairmurtuzas@gmail.com>
2024-07-01 19:52:38 +0300

    Video trying

--- components/Video.js
+++ components/Video.js
@@ -1,22 +1,22 @@
-import { react } from "react";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import Video from "next-video";
+import getStarted from "/videos/get-started.mp4";
 
-const Video = () => {
+const Videos = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <video
-        className="w-full h-full object-cover brightness-[0.9]"
-        autoPlay
-        muted
-        loop
-        preload="auto"
-      >
-        <source src="/final-images/12.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+    // <div className="w-full h-screen flex items-center justify-center">
+    //   <video
+    //     className="w-full h-full object-cover brightness-[0.9]"
+    //     autoPlay
+    //     muted
+    //     loop
+    //     preload="auto"
+    //   >
+    //     <source src="/final-images/12.mp4" type="video/mp4" />
+    //     Your browser does not support the video tag.
+    //   </video>
+    // </div>
+    <Video src={getStarted} />
   );
 };
 
-export default Video;
+export default Videos;
