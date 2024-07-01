cf7716653c422ca88c30b6babef3a218323e5c0b
Syed Omair Murtuza <omairmurtuzas@gmail.com>
2024-07-02 01:39:03 +0300

    qq

--- components/Video.js
+++ components/Video.js
@@ -1,7 +1,14 @@
 const Vid = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <video className="w-full h-full object-cover" autoPlay muted loop>
+      <video
+        width={2000}
+        height={2000}
+        className="w-full h-full object-cover"
+        // autoPlay
+        // muted
+        // loop
+      >
         <source src="/final-images/marqi2.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
