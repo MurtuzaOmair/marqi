4e84c376d6e8943788e589146b518ce307889333
Syed Omair Murtuza <omairmurtuzas@gmail.com>
2024-07-02 00:58:39 +0300

    final video now

--- components/Video.js
+++ components/Video.js
@@ -4,35 +4,20 @@ import heroVideo from "@/videos/11.webm";
 const Vid = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      {/* <video
-        width={2000}
-        height={2000}
-        style={{ width: "100vw", height: "500px", objectFit: "cover" }}
-        autoPlay
-        muted
-        loop
-      >
-        <source src="/final-images/11.webm" type="video/webm" />
+      <video className="w-full h-full object-cover" autoPlay muted loop>
+        <source src="/final-images/marqi.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      {/* 
       <video
         autoPlay
         loop
         style={{ width: "100vw", height: "500px", objectFit: "cover" }}
       >
         <source src="/final-images/11.webm" type="video/webm" />
-      </video> */}
-      <video
-        controls
-        width="640"
-        height="360"
-        // autoPlay
-        // loop
-        className="rounded-lg shadow-lg"
-      >
-        <source src="/final-images/13.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      </video> 
+      */}
+
       {/* <Video src={heroVideo} /> */}
     </div>
   );
