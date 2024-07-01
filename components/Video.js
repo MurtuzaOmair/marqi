730b0c6a9a724e004b26ce99248db46f6299adfc
Syed Omair Murtuza <omairmurtuzas@gmail.com>
2024-07-02 01:35:31 +0300

    trying my best

--- components/Video.js
+++ components/Video.js
@@ -2,7 +2,7 @@ const Vid = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <video className="w-full h-full object-cover" autoPlay muted loop>
-        <source src="/final-images/marqi.mp4" type="video/mp4" />
+        <source src="/final-images/marqi2.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
