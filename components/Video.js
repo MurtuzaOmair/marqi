5b299c79f8e5bb7356c62fae7c775ee003f2ed3c
Syed Omair Murtuza <omairmurtuzas@gmail.com>
2024-07-01 20:42:23 +0300

    video try

--- components/Video.js
+++ components/Video.js
@@ -1,22 +1,22 @@
 import Video from "next-video";
-import getStarted from "/videos/get-started.mp4";
+import heroVideo from "@/videos/11.webm";
 
-const Videos = () => {
+const Vid = () => {
   return (
-    // <div className="w-full h-screen flex items-center justify-center">
-    //   <video
-    //     className="w-full h-full object-cover brightness-[0.9]"
-    //     autoPlay
-    //     muted
-    //     loop
-    //     preload="auto"
-    //   >
-    //     <source src="/final-images/12.mp4" type="video/mp4" />
-    //     Your browser does not support the video tag.
-    //   </video>
-    // </div>
-    <Video src={getStarted} />
+    <div className="w-full h-screen flex items-center justify-center">
+      <video
+        className="w-full h-full object-cover brightness-[0.9] z-[1000000000000000] "
+        autoPlay
+        muted
+        loop
+      >
+        <source src="/final-images/11.webm" type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
+
+      {/* <Video src={heroVideo} /> */}
+    </div>
   );
 };
 
-export default Videos;
+export default Vid;
