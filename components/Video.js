61e92ef33d095fb53fd6362a8888cacde3028d2b
Syed Omair Murtuza <omairmurtuzas@gmail.com>
2024-09-10 06:38:18 +0300

    Video

--- components/Video.js
+++ components/Video.js
@@ -1,23 +1,9 @@
-import React, { useRef, useEffect } from "react";
-import { gsap } from "gsap";
+import React from "react";
 
 const Video = () => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    if (videoRef.current) {
-      gsap.fromTo(
-        videoRef.current,
-        { opacity: 0 },
-        { opacity: 1, duration: 1, ease: "power2.inOut" }
-      );
-    }
-  }, []);
-
   return (
     <div className="w-full h-[80vh] md:h-screen flex items-center justify-center overflow-hidden">
       <video
-        ref={videoRef}
         className="w-full h-full object-cover"
         autoPlay
         muted
