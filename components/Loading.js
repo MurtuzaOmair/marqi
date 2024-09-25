c76f6dc9682ad4c3240506c8681b9cd5285cb8a7
Syed Omair Murtuza <omairmurtuzas@gmail.com>
2024-09-25 06:13:17 +0300

    loader

--- components/Loading.js
+++ components/Loading.js
@@ -1,14 +1,30 @@
-// components/Loading.js
-import { useState, useEffect } from "react";
-import { gsap } from "gsap";
+import React, { useState, useEffect } from "react";
 
-const Loading = ({ loadingProgress }) => {
+const Loading = () => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          clearInterval(timer);
+          return 100;
+        }
+        const newProgress = oldProgress + 1;
+        return Math.min(newProgress, 100);
+      });
+    }, 20);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div className="flex justify-center items-center h-screen bg-[#fcf3ff]">
-      <div className="text-[#51375b] text-6xl font-normal">
-        {loadingProgress}%
-      </div>
+      <div className="text-[#51375b] text-6xl font-normal">{progress}%</div>
     </div>
   );
 };
+
 export default Loading;
--- pages/index.js
+++ pages/index.js
@@ -11,41 +11,18 @@ import React, { useEffect, useState } from "react";
 import Loading from "components/Loading";
 
 export default function Home() {
-  const [loadedComponents, setLoadedComponents] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
-  const components = [
-    Navbar,
-    Video,
-    Service,
-    Hero,
-    AboutUs,
-    Gallery,
-    OurProjects,
-    GetInTouch,
-    Footer,
-  ];
-
   useEffect(() => {
-    const loadComponent = async (index) => {
-      if (index < components.length) {
-        // Simulate component loading
-        await new Promise((resolve) => setTimeout(resolve, 300));
-        setLoadedComponents((prevLoaded) => prevLoaded + 1);
-        loadComponent(index + 1);
-      } else {
-        setIsLoading(false);
-      }
-    };
+    // Simulate a loading delay
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Adjust this time as needed
 
-    loadComponent(0);
+    return () => clearTimeout(timer);
   }, []);
 
-  const loadingProgress = Math.round(
-    (loadedComponents / components.length) * 100
-  );
-
-  if (isLoading) return <Loading loadingProgress={loadingProgress} />;
+  if (isLoading) return <Loading />;
 
   return (
     <div className=" overflow-clip ">
