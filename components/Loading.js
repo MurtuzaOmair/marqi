d8900a181ed9f63ab2dbc92f63000945ac1b60b1
Syed Omair Murtuza <omairmurtuzas@gmail.com>
2024-12-03 02:32:30 +0300

    Loading changes

--- components/Loading.js
+++ components/Loading.js
@@ -1,28 +1,17 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 
-const Loading = () => {
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((oldProgress) => {
-        if (oldProgress === 100) {
-          clearInterval(timer);
-          return 100;
-        }
-        const newProgress = oldProgress + 1;
-        return Math.min(newProgress, 100);
-      });
-    }, 20);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
+const Loading = ({ progress }) => {
   return (
-    <div className="flex justify-center items-center h-screen bg-[#fcf3ff]">
-      <div className="text-[#51375b] text-6xl font-normal">{progress}%</div>
+    <div className="fixed inset-0 z-50 flex justify-center items-center bg-[#fcf3ff]">
+      <div
+        className="text-[#51375b] text-6xl font-normal"
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        {Math.round(progress)}%
+      </div>
     </div>
   );
 };
--- components/PageLoader.js
+++ components/PageLoader.js
@@ -4,9 +4,19 @@ import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
 
 export default function PageLoader({ children }) {
+  const [progress, setProgress] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    const updateProgress = () => {
+      const totalResources = performance.getEntriesByType("resource").length;
+      const loadedResources = performance
+        .getEntriesByType("resource")
+        .filter((r) => r.responseEnd > 0).length;
+      const newProgress = (loadedResources / totalResources) * 100;
+      setProgress(newProgress);
+    };
+
     const handleLoad = () => {
       setIsLoading(false);
     };
@@ -15,12 +25,15 @@ export default function PageLoader({ children }) {
       handleLoad();
     } else {
       window.addEventListener("load", handleLoad);
+      const interval = setInterval(updateProgress, 100); // Update progress every 100ms
+      return () => {
+        window.removeEventListener("load", handleLoad);
+        clearInterval(interval);
+      };
     }
-
-    return () => window.removeEventListener("load", handleLoad);
   }, []);
 
-  if (isLoading) return <Loading />;
+  if (isLoading) return <Loading progress={progress} />;
 
   return <>{children}</>;
 }
