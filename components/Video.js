30ab0a5f8c4c931ee1480c4c88f1df3d1f84d1e3
Syed Omair Murtuza <omairmurtuzas@gmail.com>
2024-12-21 00:04:54 +0300

    vids changes

--- components/Video.js
+++ components/Video.js
@@ -2,19 +2,13 @@
 
 export default function Video() {
   return (
-    <div className="w-full h-[80vh] md:h-screen flex items-center justify-center overflow-hidden">
-      <video
-        src={"/final-media/main/video/fhd.mp4"}
-        autoPlay
-        muted
-        loop
-        playsInline
-        style={{
-          width: "100vw", // Full width of the viewport
-          height: "100vh", // Full height of the viewport
-          objectFit: "cover", // Covers the entire screen
-        }}
-      ></video>
-    </div>
+    <video
+      src={"/final-media/main/video/fhd.mp4"}
+      autoPlay
+      muted
+      loop
+      playsInline
+      className="w-full h-[80vh] md:h-screen flex items-center justify-center overflow-hidden object-cover"
+    ></video>
   );
 }
